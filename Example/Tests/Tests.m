cbd5da4f8c0d4ee3c4a8a31359c00b0cb3e0906b
ravendeng <dlfkid@icloud.com>
2022-11-29 00:11:19 +0800

    feat: add unit tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -7,8 +7,11 @@
 //
 
 @import XCTest;
+#import <RVListView/RVListViewModel.h>
 
 @interface Tests : XCTestCase
+
+@property RVListViewModel *listModel;
 
 @end
 
@@ -17,18 +20,51 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.listModel = [[RVListViewModel alloc] init];
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.listModel = nil;
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testTitleWidth1
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    CGFloat maxWidth = 120;
+    self.listModel.title = @"RVListView";
+    self.listModel.maxWidth = maxWidth;
+    self.listModel.useFlexibleWidth = NO;
+    CGFloat titleWidth = [self.listModel titleWidth];
+    XCTAssertTrue(titleWidth == maxWidth, @"Title width is not working as assumed");
+}
+
+- (void)testTitleWidth2
+{
+    CGFloat maxWidth = 300;
+    self.listModel.useFlexibleWidth = YES;
+    self.listModel.maxWidth = maxWidth;
+    self.listModel.title = @"RVListView";
+    self.listModel.font = [UIFont boldSystemFontOfSize:18];
+    self.listModel.title = @"We are the emperor's chosen.";
+    CGSize size = [@"We are the emperor's chosen." sizeWithAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIFont boldSystemFontOfSize:18], NSFontAttributeName, nil]];
+    int tempTitleWidth = (int)size.width;
+    int titleWidth = (int)[self.listModel titleWidth];
+    XCTAssertTrue(titleWidth == tempTitleWidth, @"Title width is not working as assumed");
+}
+
+- (void)testTitleWidth3
+{
+    CGFloat maxWidth = 120;
+    self.listModel.useFlexibleWidth = YES;
+    self.listModel.maxWidth = maxWidth;
+    self.listModel.font = [UIFont systemFontOfSize:12];
+    NSString *longTItle = @"My preferred option: I ended up putting the Swift Package in an Xcode workspace. By this I mean dragging the root directory containing the Swift Package (and also the Workspace) into the Xcode project navigator/ workspace.";
+    self.listModel.title = longTItle;
+    CGSize size = [longTItle sizeWithAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIFont systemFontOfSize:12], NSFontAttributeName, nil]];
+    CGFloat tempTitleWidth = size.width;
+    CGFloat titleWidth = [self.listModel titleWidth];
+    XCTAssertTrue(titleWidth < tempTitleWidth, @"Title width is not working as assumed");
 }
 
 @end
